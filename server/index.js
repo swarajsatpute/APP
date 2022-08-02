b14b15d9f20a469ff7a9d9c47ea9593755d2b27f
swarajsatpute <swarajsatpute.pub@gmail.com>
2022-08-02 12:42:46 +0530

    c2

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public'))
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
-const port = process.env.PORT || 5005
+// const port = process.env.PORT || 5005
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`server start @ ${port}`)
 })
