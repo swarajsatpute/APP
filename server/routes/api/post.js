2c3dd07dab92c5e962f4235adebf87959df12077
swarajsatpute <swarajsatpute.pub@gmail.com>
2022-08-05 20:08:16 +0530

    version 2

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -23,6 +23,61 @@ router.delete('/:id', async (req, res) => {
   await posts.deleteOne({ _id: new mongodb.ObjectId(req.params.id) })
   res.status(200).send()
 })
+//find
+//========================================
+// router.put('/:id', async (req, res) => {
+//   const { id } = req.params
+//   console.log(req.body, req.params)
+//   // console.log(new mongodb.ObjectId(req.params.id))
+//   const posts = await loadPostCollections()
+//   try {
+//     const response = await posts.findOne({
+//       _id: new mongodb.ObjectId(req.params.id),
+//     })
+//     if (!response) throw Error('Problem in Update() => id not found')
+//     const updated = req.params
+//     //...response._doc,...req.body
+//     res.status(200).json(updated)
+//   } catch (error) {
+//     res.status(500).json({ message: error.message })
+//   }
+// })
+//update
+//==========================================
+// router.put('/:id', async (req, res) => {
+//   const posts = await loadPostCollections()
+//   const id = req.params
+//   const text = req.body
+//   console.log(id, { $set: text })
+//   const response = await posts.updateOne(
+//     { _id: new mongodb.ObjectId(req.params.id) },
+//     { $set: text }
+//   )
+//   const updated = { ...response }
+//   //...response._doc,...req.body
+//   res.status(200).json(updated)
+// })
+// //========================================
+router.put('/:id', async (req, res) => {
+  const posts = await loadPostCollections()
+  const id = req.params
+  const text = req.body
+  // console.log(query)
+  //({$set:{ text: 'Edit Template =>TBR' }})
+  try {
+    const response = await posts.updateOne(
+      { _id: new mongodb.ObjectId(req.params.id) },
+      { $set: text }
+    )
+    if (!response) throw Error('Problem in Update() => id not found')
+    const updated = { ...response }
+    //...response._doc,...req.body
+    res.status(200).json(updated)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+//========================================
 // --------------------------------og
 async function loadPostCollections() {
   const client = await mongodb.MongoClient.connect(
